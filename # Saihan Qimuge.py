9e9e3469cde9c317bed290ba942ddb80772e9957
Thatguy02496 <103608485+Thatguy02496@users.noreply.github.com>
2022-09-05 13:10:00 +1200

    Add files via upload

---
+++ # Saihan Qimuge.py
@@ -0,0 +1,14 @@
+# Saihan Qimuge
+# variables, show data types
+
+#declaration
+name = "Saihan"
+numEyes = 2
+height = 1
+hasBrain = True
+
+# output  data type to screen 
+print(type(name))
+print(type(numEyes))
+print(type(height))
+print(type(hasBrain))
---
+++ input_from_user.py
@@ -0,0 +1,6 @@
+ # Saiahn Qimuge
+ # User Input
+
+ name = input("Saihan")
+
+ print("Hello,",name, "I hope you have a good day.")
